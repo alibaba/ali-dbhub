cfecd3dbaed49efc170e811facf435ed31209bbd
moji <jcy246559@alibaba-inc.com>
2022-09-18 15:11:56 +0800

    数据源SQL

---
+++ data-ops-server/data-ops-server-start/src/main/resources/db/data_source_schema.sql
@@ -0,0 +1,16 @@
+/******************************************/
+/*   TableName = data_source   */
+/******************************************/
+CREATE TABLE IF NOT EXISTS `data_source` (
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `gmt_create` datetime NOT NULL COMMENT '创建时间',
+    `gmt_modified` datetime NOT NULL COMMENT '修改时间',
+    `alias` varchar(128) DEFAULT NULL COMMENT '别名',
+    `url` varchar(1024) DEFAULT NULL COMMENT '连接地址',
+    `user` varchar(128) DEFAULT NULL COMMENT '用户名',
+    `password` varchar(256) DEFAULT NULL COMMENT '密码',
+    `type` varchar(32) DEFAULT NULL COMMENT '数据库类型',
+    `env_type` varchar(32) DEFAULT NULL COMMENT '环境类型'
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='数据源连接表'
+;
