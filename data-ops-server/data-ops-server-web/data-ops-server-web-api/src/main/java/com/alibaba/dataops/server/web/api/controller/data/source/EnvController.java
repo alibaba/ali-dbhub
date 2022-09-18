3bcfe446d5609d04025bfb0f5d40b7df1c15b32f
moji <jcy246559@alibaba-inc.com>
2022-09-18 14:10:50 +0800

    环境变量添加

---
+++ data-ops-server/data-ops-server-web/data-ops-server-web-api/src/main/java/com/alibaba/dataops/server/web/api/controller/data/source/EnvController.java
@@ -0,0 +1,31 @@
+package com.alibaba.dataops.server.web.api.controller.data.source;
+
+import com.alibaba.dataops.server.tools.base.wrapper.result.ListResult;
+import com.alibaba.dataops.server.web.api.controller.data.source.vo.EnvVO;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 环境打标服务类
+ *
+ * @author moji
+ * @version EnvController.java, v 0.1 2022年09月18日 14:04 moji Exp $
+ * @date 2022/09/18
+ */
+@RequestMapping("/api/env")
+@RestController
+public class EnvController {
+
+    /**
+     * 查询我建立的数据库连接
+     *
+     * @return
+     */
+    @GetMapping("/list")
+    public ListResult<EnvVO> list() {
+        return null;
+    }
+
+}
