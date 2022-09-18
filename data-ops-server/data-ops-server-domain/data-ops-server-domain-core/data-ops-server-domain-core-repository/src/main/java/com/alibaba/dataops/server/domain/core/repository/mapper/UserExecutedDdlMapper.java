425f3943198c89cef1f11cc0b00da95862ae5ba2
moji <jcy246559@alibaba-inc.com>
2022-09-18 17:27:15 +0800

    国际化

---
+++ data-ops-server/data-ops-server-domain/data-ops-server-domain-core/data-ops-server-domain-core-repository/src/main/java/com/alibaba/dataops/server/domain/core/repository/mapper/UserExecutedDdlMapper.java
@@ -0,0 +1,16 @@
+package com.alibaba.dataops.server.domain.core.repository.mapper;
+
+import com.alibaba.dataops.server.domain.core.repository.entity.UserExecutedDdlDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 我的执行记录表 Mapper 接口
+ * </p>
+ *
+ * @author data-ops
+ * @since 2022-09-18
+ */
+public interface UserExecutedDdlMapper extends BaseMapper<UserExecutedDdlDO> {
+
+}
