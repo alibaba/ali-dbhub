59caec412a484d5cce0fc1f5ec1bd4581327c2eb
moji <jcy246559@alibaba-inc.com>
2022-09-25 16:44:36 +0800

    用户保存ddl服务层实现

---
+++ data-ops-server/data-ops-server-domain/data-ops-server-domain-core/data-ops-server-domain-core-api/src/main/java/com/alibaba/dataops/server/domain/core/api/param/UserSavedDdlPageQueryParam.java
@@ -0,0 +1,34 @@
+package com.alibaba.dataops.server.domain.core.api.param;
+
+import com.alibaba.dataops.server.tools.base.wrapper.param.PageQueryParam;
+
+import lombok.Data;
+
+/**
+ * @author moji
+ * @version UserSavedDdlPageQueryParam.java, v 0.1 2022年09月25日 14:05 moji Exp $
+ * @date 2022/09/25
+ */
+@Data
+public class UserSavedDdlPageQueryParam extends PageQueryParam {
+
+    /**
+     * 数据源连接ID
+     */
+    private Long dataSourceId;
+
+    /**
+     * db名称
+     */
+    private String databaseName;
+
+    /**
+     * ddl语句状态:DRAFT/RELEASE
+     */
+    private String status;
+
+    /**
+     * 搜索关键词
+     */
+    private String searchKey;
+}
