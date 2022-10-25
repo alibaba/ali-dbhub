870e64fbc74afa3b7d2ccfcaf019c4505df7bc2a
是仪 <zhuangjiaju.zjj@alibaba-inc.com>
2022-10-25 13:57:04 +0800

    支持sql执行

--- data-ops-server/data-ops-server-domain/data-ops-server-domain-data/data-ops-server-domain-data-core/src/main/java/com/alibaba/dataops/server/domain/data/core/service/impl/JdbcTemplateDataServiceImpl.java
+++ data-ops-server/data-ops-server-domain/data-ops-server-domain-data/data-ops-server-domain-data-core/src/main/java/com/alibaba/dataops/server/domain/data/core/service/impl/JdbcTemplateDataServiceImpl.java
@@ -1,15 +1,11 @@
 package com.alibaba.dataops.server.domain.data.core.service.impl;
 
-import java.util.Map;
-
 import com.alibaba.dataops.server.domain.data.api.model.ExecuteResultDTO;
 import com.alibaba.dataops.server.domain.data.api.param.template.TemplateExecuteParam;
 import com.alibaba.dataops.server.domain.data.api.service.JdbcTemplateDataService;
 import com.alibaba.dataops.server.domain.data.core.model.JdbcDataTemplate;
 import com.alibaba.dataops.server.domain.data.core.util.DataCenterUtils;
-import com.alibaba.dataops.server.tools.base.excption.BusinessException;
 import com.alibaba.dataops.server.tools.base.wrapper.result.DataResult;
-import com.alibaba.dataops.server.tools.common.enums.ErrorEnum;
 import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.ast.statement.SQLSelectStatement;
@@ -39,17 +35,5 @@ public class JdbcTemplateDataServiceImpl implements JdbcTemplateDataService {
         } else {
             return DataResult.of(jdbcDataTemplate.update(param.getSql()));
         }
-    }
-
-    private JdbcDataTemplate jdbcDataTemplate(Long dataSourceId, Long consoleId) {
-        Map<Long, JdbcDataTemplate> jdbcDataTemplateMap = DataCenterUtils.JDBC_TEMPLATE_CACHE.get(dataSourceId);
-        if (jdbcDataTemplateMap == null) {
-            throw new BusinessException(ErrorEnum.CONSOLE_NOT_FOUND);
-        }
-        JdbcDataTemplate jdbcDataTemplate = jdbcDataTemplateMap.get(consoleId);
-        if (jdbcDataTemplate == null) {
-            throw new BusinessException(ErrorEnum.CONSOLE_NOT_FOUND);
-        }
-        return jdbcDataTemplate;
     }
 }
