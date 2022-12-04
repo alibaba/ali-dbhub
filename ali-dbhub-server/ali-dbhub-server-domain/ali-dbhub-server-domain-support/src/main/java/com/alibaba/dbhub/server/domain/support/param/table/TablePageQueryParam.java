a88b63e2fded77d2b70efc7d7c5a37e62232755b
是仪 <zhuangjiaju.zjj@alibaba-inc.com>
2022-12-04 13:45:51 +0800

    完成目录迁移

---
+++ ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/param/table/TablePageQueryParam.java
@@ -0,0 +1,38 @@
+package com.alibaba.dbhub.server.domain.support.param.table;
+
+import javax.validation.constraints.NotNull;
+
+import com.alibaba.dbhub.server.tools.base.wrapper.param.PageQueryParam;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * 分页查询表信息
+ *
+ * @author Jiaju Zhuang
+ */
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TablePageQueryParam extends PageQueryParam {
+    /**
+     * 对应数据库存储的来源id
+     */
+    @NotNull
+    private Long dataSourceId;
+
+    /**
+     * 对应的连接数据库名称
+     */
+    @NotNull
+    private String databaseName;
+
+    /**
+     * 表名
+     */
+    private String tableName;
+}
---
+++ ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/template/JdbcTemplate.java
@@ -0,0 +1,61 @@
+package com.alibaba.dbhub.server.domain.support.template;
+
+import java.sql.SQLException;
+
+import com.alibaba.dbhub.server.domain.support.model.ExecuteResult;
+import com.alibaba.dbhub.server.domain.support.model.support.JdbcDataTemplate;
+import com.alibaba.dbhub.server.domain.support.operations.JdbcOperations;
+import com.alibaba.dbhub.server.domain.support.param.template.TemplateCountParam;
+import com.alibaba.dbhub.server.domain.support.param.template.TemplateExecuteParam;
+import com.alibaba.dbhub.server.domain.support.util.DataCenterUtils;
+import com.alibaba.druid.DbType;
+import com.alibaba.druid.sql.PagerUtils;
+import com.alibaba.fastjson2.JSON;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * jdbc服务实现
+ *
+ * @author Jiaju Zhuang
+ */
+@Service
+@Slf4j
+public class JdbcTemplate implements JdbcOperations {
+
+    @Override
+    public ExecuteResult execute(TemplateExecuteParam param) {
+        JdbcDataTemplate jdbcDataTemplate = DataCenterUtils.getJdbcDataTemplate(param.getDataSourceId(),
+            param.getConsoleId(), param.getDatabaseName());
+        String sql = param.getSql();
+        ExecuteResult executeResult;
+        try {
+            executeResult = jdbcDataTemplate.execute(sql, param.getPageSize());
+        } catch (SQLException e) {
+            log.warn("执行sql:{}异常", JSON.toJSONString(param), e);
+            executeResult = ExecuteResult.builder()
+                .sql(param.getSql())
+                .success(Boolean.FALSE)
+                .message(e.getMessage())
+                .build();
+        }
+        return executeResult;
+    }
+
+    @Override
+    public long count(TemplateCountParam param) {
+        JdbcDataTemplate jdbcDataTemplate = DataCenterUtils.getJdbcDataTemplate(param.getDataSourceId(),
+            param.getConsoleId(), param.getDatabaseName());
+        DbType dbType = DataCenterUtils.getDruidDbTypeByDataSourceId(param.getDataSourceId());
+
+        String countSql = PagerUtils.count(param.getSql(), dbType);
+        ExecuteResult executeResult = null;
+        try {
+            executeResult = jdbcDataTemplate.execute(countSql, null);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return executeResult.getDataList().get(0).get(0).getBigDecimalValue().longValue();
+    }
+}
