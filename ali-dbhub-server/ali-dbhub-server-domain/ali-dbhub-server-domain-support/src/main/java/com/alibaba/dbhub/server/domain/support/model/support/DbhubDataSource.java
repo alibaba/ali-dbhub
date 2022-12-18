ee73ec28ed0a84a1c3a3d2a5ae17a89c75fbf037
Jiaju Zhuang <zhuangjiaju@qq.com>
2022-12-18 18:03:05 +0800

    修改测试案例

--- ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/model/support/DbhubDataSource.java
+++ ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/model/support/DbhubDataSource.java
@@ -12,10 +12,6 @@ import javax.validation.constraints.NotNull;
 
 import com.alibaba.dbhub.server.domain.support.enums.DbTypeEnum;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.ObjectUtils;
 
@@ -62,6 +58,7 @@ public class DbhubDataSource implements DataSource {
      * url不包含database
      */
     private String urlWithOutDatabase;
+
     @Override
     public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url.trim(), username, password);
@@ -144,6 +141,9 @@ public class DbhubDataSource implements DataSource {
      */
     public void setUrl(String url) {
         this.url = url;
+        if (this.dbType != DbTypeEnum.MYSQL && this.dbType != DbTypeEnum.POSTGRESQL) {
+            return;
+        }
         if (!ObjectUtils.isEmpty(url)) {
             //jdbc:postgresql://localhost:5432/postgres
             String[] array = getUrl().split(":");
@@ -157,7 +157,7 @@ public class DbhubDataSource implements DataSource {
                     if (isDigit == true) {
                         if (!Character.isDigit(c)) {
                             isDigit = false;
-                        }else {
+                        } else {
                             sb1.append(c);
                         }
                     } else {
@@ -169,7 +169,7 @@ public class DbhubDataSource implements DataSource {
                     this.database = s;
                 }
                 this.port = Integer.parseInt(sb1.toString());
-                this.urlWithOutDatabase = array[0]+":"+array[1]+":"+array[2]+":"+port;
+                this.urlWithOutDatabase = array[0] + ":" + array[1] + ":" + array[2] + ":" + port;
             }
         }
     }
@@ -226,7 +226,7 @@ public class DbhubDataSource implements DataSource {
      */
     public void setDatabase(String database) {
         this.database = database;
-        if(!ObjectUtils.isEmpty(this.urlWithOutDatabase) && !ObjectUtils.isEmpty(this.database) ) {
+        if (!ObjectUtils.isEmpty(this.urlWithOutDatabase) && !ObjectUtils.isEmpty(this.database)) {
             this.url = this.urlWithOutDatabase + "/" + database;
         }
     }
--- ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/template/ConsoleTemplate.java
+++ ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/template/ConsoleTemplate.java
@@ -3,9 +3,7 @@ package com.alibaba.dbhub.server.domain.support.template;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Map;
-import java.util.Objects;
 
-import com.alibaba.dbhub.server.domain.support.enums.DbTypeEnum;
 import com.alibaba.dbhub.server.domain.support.model.support.DbhubDataSource;
 import com.alibaba.dbhub.server.domain.support.model.support.JdbcAccessor;
 import com.alibaba.dbhub.server.domain.support.model.support.JdbcDataTemplate;
@@ -14,12 +12,13 @@ import com.alibaba.dbhub.server.domain.support.param.console.ConsoleCloseParam;
 import com.alibaba.dbhub.server.domain.support.param.console.ConsoleCreateParam;
 import com.alibaba.dbhub.server.domain.support.util.DataCenterUtils;
 import com.alibaba.dbhub.server.tools.base.excption.BusinessException;
+import com.alibaba.dbhub.server.tools.base.excption.CommonErrorEnum;
+import com.alibaba.dbhub.server.tools.base.excption.SystemException;
 import com.alibaba.dbhub.server.tools.common.enums.ErrorEnum;
 
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
 /**
  * 控制台服务
@@ -32,56 +31,62 @@ public class ConsoleTemplate implements ConsoleOperations {
 
     @Override
     public void create(ConsoleCreateParam param) {
-        DbhubDataSource dbhubDataSource = DataCenterUtils.JDBC_ACCESSOR_MAP.get(param.getDataSourceId()).getDbhubDataSource();
+        JdbcAccessor jdbcAccessor = DataCenterUtils.JDBC_ACCESSOR_MAP.get(param.getDataSourceId());
+        if (jdbcAccessor == null) {
+            throw new BusinessException(ErrorEnum.DATA_SOURCE_NOT_FOUND);
+        }
+        DbhubDataSource dbhubDataSource = jdbcAccessor.getDbhubDataSource();
         if (dbhubDataSource == null) {
             throw new BusinessException(ErrorEnum.DATA_SOURCE_NOT_FOUND);
         }
 
         Long consoleId = param.getConsoleId();
+        switch (dbhubDataSource.getDbType()) {
+            case MYSQL, H2 -> {
+                // 尝试关闭
+                close(ConsoleCloseParam.builder().dataSourceId(param.getDataSourceId()).consoleId(consoleId).build());
+                Connection connection;
+                try {
+                    connection = dbhubDataSource.getConnection();
+                } catch (SQLException e) {
+                    throw new BusinessException("连接数据库异常", e);
+                }
+                // 放入连接队列
+                Map<Long, JdbcDataTemplate> jdbcDataTemplateMap = DataCenterUtils.JDBC_TEMPLATE_CACHE.computeIfAbsent(
+                    param.getDataSourceId(), key -> Maps.newConcurrentMap());
+                JdbcDataTemplate jdbcDataTemplate = new JdbcDataTemplate(param.getDataSourceId(), consoleId, connection,
+                    dbhubDataSource);
+                jdbcDataTemplateMap.put(consoleId, jdbcDataTemplate);
 
-        if(DbTypeEnum.MYSQL.name().equals(dbhubDataSource.getDbType().name())) {
-            // 尝试关闭
-            close(ConsoleCloseParam.builder().dataSourceId(param.getDataSourceId()).consoleId(consoleId).build());
-            Connection connection;
-            try {
-                connection = dbhubDataSource.getConnection();
-            } catch (SQLException e) {
-                throw new BusinessException("连接数据库异常", e);
+                // 切换到当前database
+                try {
+                    jdbcDataTemplate.execute("use " + param.getDatabaseName() + ";", null);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
             }
-            // 放入连接队列
-            Map<Long, JdbcDataTemplate> jdbcDataTemplateMap = DataCenterUtils.JDBC_TEMPLATE_CACHE.computeIfAbsent(
-                param.getDataSourceId(), key -> Maps.newConcurrentMap());
-            JdbcDataTemplate jdbcDataTemplate = new JdbcDataTemplate(param.getDataSourceId(), consoleId, connection,
-                dbhubDataSource);
-            jdbcDataTemplateMap.put(consoleId, jdbcDataTemplate);
-
-            // 切换到当前database
-            try {
-                jdbcDataTemplate.execute("use " + param.getDatabaseName() + ";", null);
-            } catch (SQLException e) {
-                e.printStackTrace();
-                //throw new RuntimeException(e);
+            case POSTGRESQL -> {
+                close(ConsoleCloseParam.builder().dataSourceId(param.getDataSourceId()).consoleId(consoleId).build());
+                if (!param.getDatabaseName().equals(dbhubDataSource.getDatabase())) {
+                    dbhubDataSource.setDatabase(param.getDatabaseName());
+                }
+                Connection connection;
+                try {
+                    connection = dbhubDataSource.getConnection();
+                } catch (SQLException e) {
+                    throw new BusinessException("连接数据库异常", e);
+                }
+                // 放入连接队列
+                Map<Long, JdbcDataTemplate> jdbcDataTemplateMap = DataCenterUtils.JDBC_TEMPLATE_CACHE.computeIfAbsent(
+                    param.getDataSourceId(), key -> Maps.newConcurrentMap());
+                JdbcDataTemplate jdbcDataTemplate = new JdbcDataTemplate(param.getDataSourceId(), consoleId, connection,
+                    dbhubDataSource);
+                jdbcDataTemplateMap.put(consoleId, jdbcDataTemplate);
+                DataCenterUtils.JDBC_ACCESSOR_MAP.put(param.getDataSourceId(),
+                    new JdbcAccessor(param.getDataSourceId(), dbhubDataSource));
             }
-        }else if(DbTypeEnum.POSTGRESQL.name().equals(dbhubDataSource.getDbType().name())){
-            close(ConsoleCloseParam.builder().dataSourceId(param.getDataSourceId()).consoleId(consoleId).build());
-            if(!param.getDatabaseName().equals(dbhubDataSource.getDatabase())){
-                dbhubDataSource.setDatabase(param.getDatabaseName());
-            }
-            Connection connection;
-            try {
-                connection = dbhubDataSource.getConnection();
-            } catch (SQLException e) {
-                throw new BusinessException("连接数据库异常", e);
-            }
-            // 放入连接队列
-            Map<Long, JdbcDataTemplate> jdbcDataTemplateMap = DataCenterUtils.JDBC_TEMPLATE_CACHE.computeIfAbsent(
-                param.getDataSourceId(), key -> Maps.newConcurrentMap());
-            JdbcDataTemplate jdbcDataTemplate = new JdbcDataTemplate(param.getDataSourceId(), consoleId, connection,
-                dbhubDataSource);
-            jdbcDataTemplateMap.put(consoleId, jdbcDataTemplate);
-            DataCenterUtils.JDBC_ACCESSOR_MAP.put(param.getDataSourceId(),new JdbcAccessor(param.getDataSourceId(),dbhubDataSource));
+            default -> throw new SystemException(CommonErrorEnum.PARAM_ERROR);
         }
-
 
     }
 
@@ -89,7 +94,6 @@ public class ConsoleTemplate implements ConsoleOperations {
     public void close(ConsoleCloseParam param) {
         Map<Long, JdbcDataTemplate> jdbcDataTemplateMap = DataCenterUtils.JDBC_TEMPLATE_CACHE.get(
             param.getDataSourceId());
-
 
         if (jdbcDataTemplateMap == null) {
             log.info("数据库连接:{}不需要关闭", param.getDataSourceId());
@@ -106,10 +110,10 @@ public class ConsoleTemplate implements ConsoleOperations {
             log.error("关闭数据库连接异常", e);
         }
 
-        JdbcAccessor  jdbcAccessor =   DataCenterUtils.JDBC_ACCESSOR_MAP.get( param.getDataSourceId());
-        if(jdbcAccessor == null){
+        JdbcAccessor jdbcAccessor = DataCenterUtils.JDBC_ACCESSOR_MAP.get(param.getDataSourceId());
+        if (jdbcAccessor == null) {
             log.info("数据库连接:{}不需要关闭", param.getDataSourceId());
-        }else if(jdbcAccessor.getSqlSession()!=null){
+        } else if (jdbcAccessor.getSqlSession() != null) {
             jdbcAccessor.getSqlSession().close();
         }
 
