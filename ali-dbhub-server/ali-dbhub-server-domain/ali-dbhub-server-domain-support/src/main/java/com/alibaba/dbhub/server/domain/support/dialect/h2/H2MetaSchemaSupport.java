0bdaea58e09433cd9d4305a3fe534bfc44113b8c
Jiaju Zhuang <zhuangjiaju@qq.com>
2022-12-18 18:55:15 +0800

    修改测试案例报错

--- ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/dialect/h2/H2MetaSchemaSupport.java
+++ ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/dialect/h2/H2MetaSchemaSupport.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.alibaba.dbhub.server.domain.support.dialect.MetaSchema;
-import com.alibaba.dbhub.server.domain.support.dialect.common.model.SpiExample;
 import com.alibaba.dbhub.server.domain.support.dialect.h2.mapper.H2MetaSchemaMapper;
 import com.alibaba.dbhub.server.domain.support.enums.DbTypeEnum;
 import com.alibaba.dbhub.server.domain.support.model.Table;
@@ -21,12 +20,9 @@ import com.alibaba.dbhub.server.domain.support.model.TableIndexColumn;
 import com.alibaba.dbhub.server.tools.common.util.EasyCollectionUtils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.exceptions.PersistenceException;
 import org.apache.ibatis.session.SqlSession;
-import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-
-import static com.alibaba.dbhub.server.domain.support.dialect.common.SQLKeyConst.H2_ALTER_TABLE_SIMPLE;
-import static com.alibaba.dbhub.server.domain.support.dialect.common.SQLKeyConst.H2_CREATE_TABLE_SIMPLE;
 
 /**
  * @author jipengfei
@@ -41,6 +37,7 @@ public class H2MetaSchemaSupport implements MetaSchema<Table> {
     public List<String> showDatabases() {
         return getMapper().showDatabases();
     }
+
     public H2MetaSchemaSupport(SqlSession sqlSession) {
         this.sqlSession = sqlSession;
     }
@@ -52,33 +49,41 @@ public class H2MetaSchemaSupport implements MetaSchema<Table> {
     }
 
     @Override
-    public String showCreateTable( String databaseName, String schemaName, String tableName) {
-        return getMapper().showCreateTable(databaseName, tableName);
+    public String showCreateTable(String databaseName, String schemaName, String tableName) {
+        try {
+            return getMapper().showCreateTable(databaseName, tableName);
+        } catch (PersistenceException e) {
+            // 这里有个坑 就是 h2的内存模式无法获取建表语句
+            // 报错直接返回空
+            // 想办法看看能不能解决
+            log.warn("h2查询建表语句失败", e);
+            return null;
+        }
     }
 
     @Override
-    public void dropTable( String databaseName, String schemaName, String tableName) {
+    public void dropTable(String databaseName, String schemaName, String tableName) {
         getMapper().dropTable(databaseName, tableName);
     }
 
     @Override
-    public int queryTableCount( String databaseName, String schemaName) {
+    public int queryTableCount(String databaseName, String schemaName) {
         return getMapper().selectTableCount(databaseName).intValue();
     }
 
     @Override
-    public List<Table> queryTableList( String databaseName, String schemaName, int pageNo,
+    public List<Table> queryTableList(String databaseName, String schemaName, int pageNo,
         int pageSize) {
         return getMapper().selectTables(databaseName, pageSize, pageNo <= 1 ? 0 : (pageNo - 1) * pageSize);
     }
 
     @Override
-    public Table queryTable( String databaseName, String schemaName, String tableName) {
+    public Table queryTable(String databaseName, String schemaName, String tableName) {
         return null;
     }
 
     @Override
-    public List<TableColumn> queryColumnList( String databaseName, String schemaName,
+    public List<TableColumn> queryColumnList(String databaseName, String schemaName,
         List<String> tableNames) {
         return tableNames.stream().map(tableName -> getMapper().selectColumns(databaseName, tableName))
             .flatMap(Collection::stream)
@@ -86,7 +91,7 @@ public class H2MetaSchemaSupport implements MetaSchema<Table> {
     }
 
     @Override
-    public List<TableIndex> queryIndexList( String databaseName, String schemaName,
+    public List<TableIndex> queryIndexList(String databaseName, String schemaName,
         List<String> tableNames) {
         List<TableIndex> indexList = new ArrayList<>();
         H2MetaSchemaMapper mapper = getMapper();
