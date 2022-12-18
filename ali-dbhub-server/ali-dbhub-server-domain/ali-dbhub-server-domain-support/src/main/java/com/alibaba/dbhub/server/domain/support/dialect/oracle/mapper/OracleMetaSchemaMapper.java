5421c69f1769bb34aeb9c90eb5ab2f3d17d72ed8
jipengfei.jpf <jipengfei.jpf@alibaba-inc.com>
2022-12-18 20:50:47 +0800

    add oracle

---
+++ ali-dbhub-server/ali-dbhub-server-domain/ali-dbhub-server-domain-support/src/main/java/com/alibaba/dbhub/server/domain/support/dialect/oracle/mapper/OracleMetaSchemaMapper.java
@@ -0,0 +1,80 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package com.alibaba.dbhub.server.domain.support.dialect.oracle.mapper;
+
+import java.util.List;
+
+import com.alibaba.dbhub.server.domain.support.dialect.postgresql.model.PostgresqlColumn;
+import com.alibaba.dbhub.server.domain.support.dialect.postgresql.model.PostgresqlTableIndex;
+import com.alibaba.dbhub.server.domain.support.model.Table;
+import com.alibaba.dbhub.server.domain.support.model.TableColumn;
+import com.alibaba.dbhub.server.domain.support.model.TableIndexColumnUnion;
+
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author jipengfei
+ * @version : OracleMetaSchemaMapper.java, v 0.1 2022年12月13日 17:38 jipengfei Exp $
+ */
+public interface OracleMetaSchemaMapper {
+
+    /**
+     * 查询Database
+     *
+     * @return
+     */
+    List<String> showDatabases();
+
+    /**
+     * 查询所有表中所有列信息
+     *
+     * @param tableName
+     * @return
+     */
+    List<TableColumn> selectColumns( @Param("tableName") String tableName);
+
+    /**
+     * 删除表
+     *
+     * @param tableName
+     */
+    void dropTable(@Param("tableName") String tableName);
+
+    /**
+     * 查询所有的表
+     *
+     * @param tableSchema
+     * @param start
+     * @param end
+     * @return
+     */
+    List<Table> selectTables(@Param("tableSchema") String tableSchema, @Param("start") long start,
+        @Param("end") long end);
+
+    /**
+     * @param tableSchema
+     * @return
+     */
+    Long selectTableCount(@Param("tableSchema") String tableSchema);
+
+    /**
+     * 查询建表语句
+     *
+     * @param tableSchema
+     * @param tableName
+     * @return
+     */
+    String showCreateTable(@Param("tableSchema") String tableSchema, @Param("tableName") String tableName);
+
+    /**
+     * 查询表索引信息
+     *
+     * @param databaseName
+     * @param tableName
+     * @return
+     */
+    List<TableIndexColumnUnion> selectTableIndexes(@Param("databaseName") String databaseName,
+        @Param("tableName") String tableName);
+}
