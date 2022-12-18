6e2335c04bc8482ce5194dda08397bfcf28f487c
单贺喜 <wb-dhx909739@alibaba-inc.com>
2022-12-18 10:14:35 +0800

    style

--- ali-dbhub-client/.umirc.ts
+++ ali-dbhub-client/.umirc.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'umi';
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 // const MonacoWebpackPlugin = require('monaco-editor-esm-webpack-plugin');
-console.log('我是umirc')
 
 const chainWebpack = (config:any, { webpack }:any) => {
   config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
@@ -81,10 +80,4 @@ export default defineConfig({
     type: 'none',
   },
   chainWebpack,
-  // proxy: {
-  //   '/api': {
-  //     'target': 'http://localhost:10824',
-  //     'changeOrigin': true
-  //   },
-  // },
 });
