5ce553eeab2166a0ab027719a04e332264f27413
单贺喜 <wb-dhx909739@alibaba-inc.com>
2023-01-11 18:43:01 +0800

    feat:表格可编辑

--- ali-dbhub-client/src/pages/demo/index.tsx
+++ ali-dbhub-client/src/pages/demo/index.tsx
@@ -1,12 +1,25 @@
-import React, { memo, useCallback, useState } from 'react';
+import React, { memo, useCallback, useEffect, useRef, useState } from 'react';
 import styles from './index.less'
 import Iconfont from '@/components/Iconfont'
 import Tabs from '@/components/Tabs';
 import ModifyTablePage from '@/pages/modify-table';
 
 function DemoPage() {
+  const list = new Array(10).fill('123');
+  const [data, setData] = useState(1);
+  const dataRef = useRef();
+  function task() {
+    console.log(1)
+  }
+
   return <div className={styles.page}>
-    <ModifyTablePage></ModifyTablePage>
+    {
+      list.map(item => {
+        <div onClick={task}>
+          {item}
+        </div>
+      })
+    }
   </div>
 }
 
