c125cf05564a9ead0b4767d4baeb2e667ce2d47a
单贺喜 <wb-dhx909739@alibaba-inc.com>
2023-04-12 15:27:34 +0800

    zancun

--- ali-dbhub-client/.umirc.test.ts
+++ ali-dbhub-client/.umirc.test.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'umi';
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const chainWebpack = (config: any, { webpack }: any) => {
   config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
@@ -7,9 +8,16 @@ const chainWebpack = (config: any, { webpack }: any) => {
       languages: ['mysql', 'pgsql', 'sql'],
     },
   ]);
+  config.plugin("umi-webpack-bundle-analyzer").use(new BundleAnalyzerPlugin({
+    analyzerMode: 'static',
+    reportFilename: 'bundle-report.html',
+    defaultSizes: 'gzip',
+    openAnalyzer: true,
+  }));
 };
 
 export default defineConfig({
   publicPath: './static/front/',
   chainWebpack,
+  dynamicImport: false,
 });
