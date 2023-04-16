6654d0c22bb7dcb3abe7bf462d9a8a9be2a110e2
moji <jcy246559@alibaba-inc.com>
2023-04-16 14:54:20 +0800

    语言转换修改

--- ali-dbhub-server/ali-dbhub-server-web/ali-dbhub-server-web-api/src/main/java/com/alibaba/dbhub/server/web/api/controller/ai/ChatController.java
+++ ali-dbhub-server/ali-dbhub-server-web/ali-dbhub-server-web-api/src/main/java/com/alibaba/dbhub/server/web/api/controller/ai/ChatController.java
@@ -291,11 +291,12 @@ public class ChatController {
         String promptType = StringUtils.isBlank(queryRequest.getPromptType()) ? PromptType.NL_2_SQL.getCode()
             : queryRequest.getPromptType();
         PromptType pType = EasyEnumUtils.getEnum(PromptType.class, promptType);
+        String ext = StringUtils.isNotBlank(queryRequest.getExt()) ? queryRequest.getExt() : "";
         String schemaProperty = CollectionUtils.isNotEmpty(tableSchemas) ? String.format(
             "### 请根据以下table properties和SQL input%s. %s\n#\n### %s SQL tables, with their properties:\n#\n# "
-                + "%s\n#\n#\n### SQL input: %s", pType.getDescription(), queryRequest.getExt(), dataSourceType,
+                + "%s\n#\n#\n### SQL input: %s", pType.getDescription(), ext, dataSourceType,
             properties, prompt) : String.format("### 请根据以下SQL input%s. %s\n#\n### SQL input: %s",
-            pType.getDescription(), queryRequest.getExt(), prompt);
+            pType.getDescription(), ext, prompt);
         switch (pType) {
             case SQL_2_SQL:
                 schemaProperty = StringUtils.isNotBlank(queryRequest.getDestSqlType()) ? String.format(
